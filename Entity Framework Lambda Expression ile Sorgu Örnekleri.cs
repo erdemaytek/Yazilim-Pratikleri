83200a526c2390ffd1d9db3826618573d9f87a8e
Erdem AYTEK <31161840+erdemaytek@users.noreply.github.com>
2020-02-02 11:24:34 +0300

    Update Entity Framework Lambda Expression ile Sorgu Örnekleri.cs

--- Entity Framework Lambda Expression ile Sorgu Örnekleri.cs
+++ Entity Framework Lambda Expression ile Sorgu Örnekleri.cs
@@ -1,69 +1,68 @@
-//db deki product tablomdaki tüm verileri listeye ata
-List<Product> plist = db.Products.ToList();
+//ToList() => Products tablomdaki tüm ürünleri getir.
+List<Product> liste = db.Products.ToList();
  
-//ürünleri isme göre listele ve listeme ata
-List<Product> plist2 = db.Products.OrderBy(p => p.ProductName).ToList();
+//OrderBy() => Products tablomdaki ürünleri isme göre (A'dan --> Z'ye) sırala sonra ToList() ile liste halinde bana getir.
+List<Product> liste = db.Products.OrderBy(p => p.ProductName).ToList();
  
-//ürünleri isme göre tersten listele
-List<Product> plist3 = db.Products.OrderByDescending(p => p.ProductName).ToList();
+//OrderByDescending() => Products tablomdaki ürünleri testen (Z'den --> A'aya) isme göre sırala sonra ToList() ile liste halinde bana getir.
+List<Product> liste = db.Products.OrderByDescending(p => p.ProductName).ToList();
 
-//ürünleri tersten listele ve ilk 5 ürünü yakalayıp listeme ata
-List<Product> plist4 = db.Products.OrderByDescending(p => p.ProductName).Take(5).ToList();
+//Take() => Ürünleri tersten listele ve Take() ile ilk 5 ürünü alıp liste halinde bana getir.
+List<Product> liste = db.Products.OrderByDescending(p => p.ProductName).Take(5).ToList();
  
-//id si 5 olan ürün yakala
-Product prdc = db.Products.FirstOrDefault(p => p.ProductID == 5);
+//FirstOrDefault() => ProductID si 5 olan ürünü alıp getir.
+Product urun = db.Products.FirstOrDefault(p => p.ProductID == 5);
  
-//id si 3 olan ürünün ADINI ver
+//ProductName => ProductID si 3 olan ürünün ProductName'ini getir.
 string ad = db.Products.FirstOrDefault(p => p.ProductID == 3).ProductName;
  
-//Kategori ID si 5 olan ürünlerin listesi
-List<Product> plist5 = db.Products.Where(p => p.CategoryID == 5).ToList();
+//Where => CategoryID si 5 olan ürünlerin listesini getir.
+List<Product> liste = db.Products.Where(p => p.CategoryID == 5).ToList();
  
-//Kategori ID si 3 ve Supplier ID si 2 olan ürünleri isme göre sırala ilk 5 imi al
-List<Product> plist6 = db.Products.Where(p => p.CategoryID == 3 && p.SupplierID == 2).OrderBy(p => p.ProductName).Take(5).ToList();
+//Kategori ID si 3 ve Supplier ID si 2 olan ürünleri isme göre sırala ilk 5 ini alıp liste olarak getir.
+List<Product> liste = db.Products.Where(p => p.CategoryID == 3 && p.SupplierID == 2).OrderBy(p => p.ProductName).Take(5).ToList();
  
-//productname a harfi geçen ürünlerin listesi
-List<Product> plist7 = db.Products.Where(p => p.ProductName.Contains("a")).ToList();
+//Contains() => ProductName de a harfi geçen ürünlerin listesini getir.
+List<Product> liste = db.Products.Where(p => p.ProductName.Contains("a")).ToList();
  
-//product name a harfi ile BAŞLAYAN ürünlerin listesi
-List<Product> plist8 = db.Products.Where(p => p.ProductName.StartsWith("a")).ToList();
+//StartsWith() => ProductName'inde a harfi ile başlayan ürünlerin listesini getir.
+List<Product> liste = db.Products.Where(p => p.ProductName.StartsWith("a")).ToList();
  
-//productname a harfi ile BİTEN ürünlerin listesi
-List<Product> plist9 = db.Products.Where(p => p.ProductName.EndsWith("a")).ToList();
+//EndsWith() => ProductName'inde a harfi ile biten ürünlerin listesini getir.
+List<Product> liste = db.Products.Where(p => p.ProductName.EndsWith("a")).ToList();
  
-//Kategori Var Mi
+//Any() => Kategori varmı? Varsa true, yoksa false döner.
 bool VarMi = db.Categories.Any();
  
-//Kategori IDsi 5 olan kategori var mı_
-bool VarMi2 = db.Category.Any(c => c.CategoryID == 5);
+//Any() => Kategori IDsi 5 olan kategori var mı?
+bool VarMi = db.Category.Any(c => c.CategoryID == 5);
  
-//İsminde 'ha' içeren ürün var mı? Küçük veya büyük harf farketmez
-bool VarMi3 = db.Product.Any(p => p.ProductName.Contains("ha"));
+//Any() => ProductName de 'ha' içeren ürün var mı? Küçük veya büyük harf farketmez.
+bool VarMi = db.Product.Any(p => p.ProductName.Contains("ha"));
  
-//Ürün dizisine atar
-Product[] pdizi = db.Products.ToArray();
+//ToArray() => Sonucu array'e çevirir.
+Product[] dizi = db.Products.ToArray();
  
-//ürün sayısı
+//Count() => Sonucun toplam sayısını verir. Burda ürün sayısını geterdi.
 int adet = db.Products.Count();
  
-//product tablomdaki QuantityPerUnit sayısı(tabloda bu kolon eğer null ise toplama eklenmeyecektir)
-int adet2 = db.Products.Count(p => p.QuantityPerUnit != null);
+//Count() => product tablomdaki QuantityPerUnit sayısı(tabloda bu kolon eğer null ise toplama eklenmeyecektir)
+int adet = db.Products.Count(p => p.QuantityPerUnit != null);
  
-//product tablomdaki ürün fiyatlarımın toplamı
+//Sum() => Product tablomdaki ürün fiyatlarının toplamını verir.
 decimal? toplamfiyat = db.Products.Sum(p => p.UnitPrice);
  
-//en pahalı ürünüm
+//Max() Product tablomdaki ürünlerden en yüksek fiyatlı olanını verir
 decimal? pahaliurun = db.Products.Max(a => a.UnitPrice);
  
 //isme göre sıraladığında ilk 5 ürünü atla kalan ürünleri listele
 List<Product> plist10 = db.Products.OrderBy(p => p.ProductName).Skip(5).ToList();
  
-//isme göre sıraladığında ilk 5 ürünü atla 10 ürünü listele
+//Skip() => ProductName'e göre sıraladığında ilk 5 ürünü atla 10 ürünü listele
 List<Product> plist11 = db.Products.OrderBy(p => p.ProductName).Skip(5).Take(10).ToList();
  
- 
-//order tablosundaki shipcountry kolonuna distinct uygular(sql karşılığı "select distinct o.ShipCountry //from Orders o")
+//Distinct() => Orders tablosundaki ShipCountry kolonuna distinct uygular(sql karşılığı "select distinct o.ShipCountry //from Orders o")
 List<string> countrylist = db.Orders.Select(a => a.ShipCountry).Distinct().ToList();
  
-//product tablosundaki primary key alanındaki değere eşdeğer product getirir(5 numaralı ProductID //değerini getirir)
+//Find() => Products tablosundaki primary key alanındaki değere eşdeğer product getirir(5 numaralı ProductID //değerini getirir)
 Product prdct = db.Products.Find(5);
